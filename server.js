65d2e5c879c2c050c920db58c5e531b61cabb2ca
alizaborker <57578671+alizaborker@users.noreply.github.com>
2025-06-29 00:17:39 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+const COMMENTS_FILE = path.join(__dirname, 'comments.json');
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// Endpoint to get comments
+app.get('/comments', (req, res) => {
+    fs.readFile(COMMENTS_FILE, (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading comments file');
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+// Endpoint to post a new comment
+app.post('/comments', (req, res) => {
+    const newComment = req.body;
+    fs.readFile(COMMENTS_FILE, (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading comments file');
+        }
+        const comments = JSON.parse(data);
+        comments.push(newComment);
+        fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 2), (err) => {
+            if (err) {
+                return res.status(500).send('Error writing to comments file');
+            }
+            res.status(201).send('Comment added');
+        });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
