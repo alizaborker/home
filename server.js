88ed15a1924c79bed8ec5ceb544b2193b39b6aa1
alizaborker <57578671+alizaborker@users.noreply.github.com>
2025-06-29 03:31:01 -0400

    Server side implementation of blog
    Blog with server side implementation

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ const app = express();
 const PORT = 3000;
 const COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
+// Serve static files (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname)));
+
 app.use(bodyParser.json());
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 // Endpoint to get comments
 app.get('/comments', (req, res) => {
@@ -22,20 +25,25 @@ app.get('/comments', (req, res) => {
 
 // Endpoint to post a new comment
 app.post('/comments', (req, res) => {
+    
     const newComment = req.body;
+    console.log(`Server /comments received a post ${newComment}`);
     fs.readFile(COMMENTS_FILE, (err, data) => {
         if (err) {
-            return res.status(500).send('Error reading comments file');
+            return res.status(500).send('{"response": "Error reading comments file}"}');
         }
         const comments = JSON.parse(data);
         comments.push(newComment);
         fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 2), (err) => {
             if (err) {
                 return res.status(500).send('Error writing to comments file');
+                console.log(`Server /comments : Error writing to comments file`); 
             }
-            res.status(201).send('Comment added');
+            res.status(201).send('{"response": "Comment added"}');
+            console.log(`Server /comments : Comment added`); 
         });
     });
+    
 });
 
 app.listen(PORT, () => {
